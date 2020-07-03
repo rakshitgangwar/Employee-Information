803be91ea27f090b2dc559a3f31eef8cbc40d76b
Rakshit <rakshitgangwar@gmail.com>
2020-07-04 00:45:04 +0530

    Tables added

--- emp.java
+++ emp.java
@@ -1,7 +1,25 @@
 class emp
 {
-public static void main()
+public static void main(String[] args)
 {
+	String x[][] = new String[8][8];
+	x[0][]={"Emp No","Emp Name","Join Date","Designation Code","Department Code",
+"Department","Basic" ,"HRA","IT"};
+	x[1][]={"1001","Ashish","01/04/2009","e","R&D","20000","8000","3000"};
+	x[2][]={"1002","Sushma","23/08/2012","c","PM","30000","12000","9000"};
+	x[3][]={"1003","Rahul","12/11/2008","k","Acct","10000","8000","1000"};
+	x[4][]={"1004","Chahat","29/01/2013","r","Front Desk","12000","6000","2000"};
+	x[5][]={"1005","Ranjan,","16/07/2005","m","Engg","50000","20000","20000"};
+	x[6][]={"1006","Suman","1/1/2000","e","Manufacturing","23000","9000","4400"};
+	x[7][]={"1007","Tanmay","12/06/2006","c","PM","29000","12000","10000"};
+
+	String y[][] = new String[6][3];
+	y[0][]={"Designation Code","Designation","DA"};
+	y[1][]={"e","Consultant","32000"};
+	y[2][]={"k","Clerk","12000"};
+	y[3][]={"r","Receptionist","15000"};
+	y[4][]={"m","Manager","40000"};
+
 
 }
 }
